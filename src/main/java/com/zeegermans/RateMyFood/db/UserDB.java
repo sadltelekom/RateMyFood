a3b6b2ecea129a6ed328b5067b48e331f1867bdc
Sebastian Alaimo Di Loro <sebastian.alaimo.di.loro@telekom.de>
2022-03-11 17:27:08 +0100

    Implemented USER DB as first example on how we want to structure our DB Classes.

--- src/main/java/com/zeegermans/RateMyFood/db/UserDB.java
+++ src/main/java/com/zeegermans/RateMyFood/db/UserDB.java
@@ -1,4 +1,53 @@
 package com.zeegermans.RateMyFood.db;
 
+import com.zeegermans.RateMyFood.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDB {
+    private Connection connection = DBConnector.getInstance().getConnection();
+
+    public List<User> getUsers( PreparedStatement preparedStatement){
+        List<User> filtered = new ArrayList<>();
+
+        try {
+            ResultSet result = preparedStatement.executeQuery();
+
+            while(result.next()) {
+                User user = new User(
+                        result.getLong("id"),
+                        result.getString("name"),
+                        result.getString("realname"),
+                        result.getString("status"),
+                        result.getString("email"),
+                        result.getString("password")
+
+                );
+                filtered.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return filtered;
+    }
+
+    public List<User> getAllUsers() {
+        String sql = "SELECT * FROM user";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            return getUsers(preparedStatement);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
 }
