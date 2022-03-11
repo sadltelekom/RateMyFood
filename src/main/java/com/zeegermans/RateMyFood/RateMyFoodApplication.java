f42e6971552feb396edd726412ed12a103fcf3a1
Sebastian Alaimo Di Loro <sebastian.alaimo.di.loro@telekom.de>
2022-03-11 15:44:13 +0100

    Started Implementing DB Connector

--- src/main/java/com/zeegermans/RateMyFood/RateMyFoodApplication.java
+++ src/main/java/com/zeegermans/RateMyFood/RateMyFoodApplication.java
@@ -1,12 +1,16 @@
 package com.zeegermans.RateMyFood;
 
+import com.zeegermans.RateMyFood.db.DBConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.sql.Connection;
+
+@SpringBootApplication(scanBasePackages = {"com.zeegermans.RateMyFood.controller"})
 public class RateMyFoodApplication {
 
 	public static void main(String[] args) {
+		Connection connection = DBConnector.getInstance().getConnection();
 		SpringApplication.run(RateMyFoodApplication.class, args);
 	}
 
--- src/main/java/com/zeegermans/RateMyFood/db/DBConnector.java
+++ src/main/java/com/zeegermans/RateMyFood/db/DBConnector.java
@@ -1,4 +1,36 @@
 package com.zeegermans.RateMyFood.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnector {
+    private static DBConnector instance;
+    private Connection connection;
+
+    public static synchronized DBConnector getInstance() {
+        if (instance == null) {
+            instance = new DBConnector();
+            return instance;
+        }
+        return instance;
+    }
+
+    private DBConnector() {
+        try {
+            this.connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/ratemyfood?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                    "foodadmin",
+                    "FeetMakeUsNotHorny"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
 }
