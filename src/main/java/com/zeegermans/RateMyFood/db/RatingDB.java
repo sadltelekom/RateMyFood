2883bbcad1831c3c5966e50939b8963886b78baf
Henry <bender3582@gmail.com>
2022-03-14 11:26:00 +0100

    bugfix whitespaces

--- src/main/java/com/zeegermans/RateMyFood/db/RatingDB.java
+++ src/main/java/com/zeegermans/RateMyFood/db/RatingDB.java
@@ -1,6 +1,7 @@
 package com.zeegermans.RateMyFood.db;
 
 import com.zeegermans.RateMyFood.model.Rating;
+import com.zeegermans.RateMyFood.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -46,7 +47,42 @@ public class RatingDB {
         return null;
     }
 
-    // ratings by recipe
-    // ratings by user
+    public List<Rating> getRatingsById(Long ratingId) {
+        String sql = "SELECT * FROM rating WHERE id=?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, ratingId);
+            return getRatings(preparedStatement);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    public List<User> getUserByRecipe(int recipeId) {
+        String sql = "SELECT rating.* " +
+                "FROM recipes " +
+                "INNER JOIN recipes_has_rating ON recipes.id=recipes_has_rating.recipes_id " +
+                "INNER JOIN rating ON recipes_has_rating.rating_id=rating.id " +
+                "WHERE recipes.id=?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, recipeId);
+            return getUsers(preparedStatement);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    // all ratings by recipe
+    // average rating by recipe
+    // all ratings by user
+    // average rating given by user
+    // given rating by user of certain recipe
 
 }
