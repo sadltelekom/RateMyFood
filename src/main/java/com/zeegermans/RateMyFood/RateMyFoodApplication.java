60ce7f4ef9e897067b6357e635289713d291a866
Henry <bender3582@gmail.com>
2022-03-14 23:31:47 +0100

    new comment incl. user and recipe connection tostring() for all model classes

--- src/main/java/com/zeegermans/RateMyFood/RateMyFoodApplication.java
+++ src/main/java/com/zeegermans/RateMyFood/RateMyFoodApplication.java
@@ -12,10 +12,6 @@ public class RateMyFoodApplication {
 
 	public static void main(String[] args) {
 		Connection connection = DBConnector.getInstance().getConnection();
-
-		CommentsDB comment = new CommentsDB();
-		System.out.println(comment.createNewComment("blabla wieder neuer Kommentar", 5l, 4l));
-
 		SpringApplication.run(RateMyFoodApplication.class, args);
 	}
 
