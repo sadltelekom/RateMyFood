97c56d50b31d488c6c9bb1b7dc228bea9b9bc6d6
Henry <bender3582@gmail.com>
2022-03-15 17:03:29 +0100

    IngredientsController done

--- src/main/java/com/zeegermans/RateMyFood/controller/IngredientsController.java
+++ src/main/java/com/zeegermans/RateMyFood/controller/IngredientsController.java
@@ -1,4 +1,35 @@
 package com.zeegermans.RateMyFood.controller;
 
+import com.zeegermans.RateMyFood.db.CommentsDB;
+import com.zeegermans.RateMyFood.db.DBConnector;
+import com.zeegermans.RateMyFood.db.IngredientsDB;
+import com.zeegermans.RateMyFood.model.Comments;
+import com.zeegermans.RateMyFood.model.Ingredients;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.util.List;
+
+@RestController
 public class IngredientsController {
+
+    private Connection connection = DBConnector.getInstance().getConnection();
+    IngredientsDB ingredients = new IngredientsDB();
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("get/ingredients/")
+    public List<Ingredients> allIngredients() {
+        return ingredients.getAllIngredients();
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("get/ingredients/{id}")
+    public List<Ingredients> ingredientsById(@PathVariable long id) {
+        return ingredients.getIngredientById(id);
+    }
+
+
 }
